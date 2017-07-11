6e0f75481e3e50e16135cf6557ee68bbfc2a10eb
gajcowan <gary.cowan@kongadev.com>
2017-07-11 22:49:57 +0100

    Add project files.

---
+++ cox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
